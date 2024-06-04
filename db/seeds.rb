1234d70ef155a77c7e13b31498bb1e3fa8138d5d
Pedro Leal <lealpedro2009@gmail.com>
2024-06-04 20:02:35 +0100

    feat: seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# destroy all records in the database if development environment
+# and create 3 users and 10 cameras
+if Rails.env.development?
+  Camera.destroy_all
+  User.destroy_all
+end
+
+# Create a 3 users for application
+emails = %w[pedro@mail.com thuthi@mail.com sahba@mail.com]
+3.times do
+  User.create!(email: emails.pop,
+               password: "password",
+               first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name)
+end
+# Create 10 cameras for application
+10.times do
+  Camera.create!(name: Faker::Device.model_name,
+                 optical_zoom: Faker::Number.between(from: 1, to: 100),
+                 price_per_day: Faker::Number.decimal(l_digits: 2),
+                 address: Faker::Address.full_address,
+                 resolution: Faker::Number.decimal(l_digits: 2),
+                 user_id: User.all.sample.id)
+end
