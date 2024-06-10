09a95ed68e1172f7a28433993b4bdaf852198bd4
Pedro Leal <lealpedro2009@gmail.com>
2024-06-10 09:11:30 +0100

    feat: multiple serach by name, address and resolution

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,10 +1,10 @@
 class CamerasController < ApplicationController
   before_action :set_camera, only: [:show]
   def index
-
     if params[:query].present?
-      @query = params[:query]
-      @cameras = Camera.where("name LIKE ?", "%#{params[:query]}%")
+      #sql_subquery = "name @@ :query OR address @@ :query"
+      #@cameras = Camera.where(sql_subquery,query: "%#{params[:query]}%")
+      @cameras = Camera.search_by_name_and_address_and_resolution(params[:query])
       # Preventing SQL Injection and Database error for
       # unknown characters
     else
@@ -48,17 +48,17 @@ class CamerasController < ApplicationController
     redirect_to cameras_path, notice: "Camera deleted!"
   end
 
-  def show 
-    @camera 
+  def show
+    @camera
   end
-  
+
 
   private
 
   def set_camera
     @camera = Camera.find(params[:id])
   end
-  
+
   def camera_params
     params.require(:camera).permit(:name, :optical_zoom, :address, :year, :resolution, :price_per_day)
   end
